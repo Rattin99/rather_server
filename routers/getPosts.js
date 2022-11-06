8be5bdc2e5b7c3d7398acc2f154533c05bc76b50
Rattin99 <rattin99m18@gmail.com>
2022-11-07 00:21:19 +0600

    or

--- routers/getPosts.js
+++ routers/getPosts.js
@@ -10,7 +10,7 @@ router.get('/posts',(req,res) =>{
 
     const {start,limit} = req.query;
 
-    if(start & limit) return;
+    if(start || limit) return;
     
 
     const sql  = `SELECT * FROM posts ORDER BY post_time DESC LIMIT ${start},${limit};`
